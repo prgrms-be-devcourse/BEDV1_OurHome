d6f3d9ba4f919efef9ece807aeb49890e79d8261
locodingve <locodingve@gmail.com>
2021-10-26 19:10:46 +0900

    [C] feat : entity 추가 (#8)

---
+++ ourhome-community/src/main/java/com/armand/ourhome/community/like/entity/Like.java
@@ -0,0 +1,28 @@
+package com.armand.ourhome.community.like.entity;
+
+import com.armand.ourhome.community.post.entity.Post;
+import com.armand.ourhome.domain.base.BaseEntity;
+import com.armand.ourhome.domain.user.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "like")
+public class Like extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false, referencedColumnName = "id")
+    private Post post;
+}
