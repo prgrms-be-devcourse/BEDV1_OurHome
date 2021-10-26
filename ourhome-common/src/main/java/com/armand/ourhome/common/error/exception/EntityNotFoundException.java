4fb76471d4a23da9f9e56da0a35f8088a4dfe409
seunghun-h <dau4723@gmail.com>
2021-10-26 15:01:37 +0900

    feat: 예외처리 핸들러

---
+++ ourhome-common/src/main/java/com/armand/ourhome/common/error/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.armand.ourhome.common.error.exception;
+
+public class EntityNotFoundException extends BusinessException{
+
+    public EntityNotFoundException(String message) {
+        super(message, ErrorCode.ENTITY_NOT_FOUND);
+    }
+}
+
