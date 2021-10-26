cc0758232c78eda723e72360d1d479272121f583
한재원 <hanjo8813@gmail.com>
2021-10-26 16:50:51 +0900

    feat : user 도메인 생성

---
+++ ourhome-domain/src/main/java/com/armand/ourhome/domain/base/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.armand.ourhome.domain.base;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)  // JPA Audit
+@MappedSuperclass
+public class BaseEntity {
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedAt;
+}
