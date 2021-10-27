1e46ecf8a7ff359254cefdb8b2738cffeec171bc
iyj6707 <iyj6707@naver.com>
2021-10-27 15:05:58 +0900

    fix: ItemRepository 에러 무시
    findAllByName()에 에러 생겨서 주석 처리 했어요.

--- ourhome-domain/src/main/java/com/armand/ourhome/domain/item/ItemRepository.java
+++ ourhome-domain/src/main/java/com/armand/ourhome/domain/item/ItemRepository.java
@@ -1,12 +1,13 @@
 package com.armand.ourhome.domain.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-  List<Item> findAllByName(String name);
+//  List<Item> findAllByName(String name);
 
   List<Item> findAllByCompany(Company company);
 
