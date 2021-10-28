a954244c73c90d5a2cacfcb703b86c63e417760f
한재원 <hanjo8813@gmail.com>
2021-10-28 14:59:35 +0900

    feat : user 도메인 생성자 추가

--- ourhome-domain/src/main/java/com/armand/ourhome/domain/user/User.java
+++ ourhome-domain/src/main/java/com/armand/ourhome/domain/user/User.java
@@ -1,10 +1,7 @@
 package com.armand.ourhome.domain.user;
 
 import com.armand.ourhome.domain.base.BaseEntity;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -34,4 +31,14 @@ public class User extends BaseEntity {
 
     @Column(name="profile_image_url", columnDefinition = "TEXT")
     private String profileImageUrl;
+
+    @Builder
+    public User(String email, String password, String nickname, String address, String description, String profileImageUrl) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.address = address;
+        this.description = description;
+        this.profileImageUrl = profileImageUrl;
+    }
 }
