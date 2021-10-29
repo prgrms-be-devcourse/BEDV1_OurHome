6fd892da7bcc8bc950aa25c312c03c27bd7cd6c8
yunyun <locodingve@gmail.com>
2021-10-30 02:08:03 +0900

    feat: 게시물 작성 때 필요한 각 type들의 enum 생성

--- ourhome-community/src/main/java/com/armand/ourhome/community/post/entity/PlaceType.java
+++ ourhome-community/src/main/java/com/armand/ourhome/community/post/entity/PlaceType.java
@@ -1,4 +1,25 @@
 package com.armand.ourhome.community.post.entity;
 
 public enum PlaceType {
+    ONEROOM("원룸"),
+    LIVINGROOM("거실"),
+    KITCHEN("주방"),
+    BATHROOM("욕실"),
+    DRESSROOM("드레스룸"),
+    VERANDA("베란다"),
+    OFFICEROOM("사무공간"),
+    FURNITURE("가구&소품"),
+    EXTERIOR("외관&기타");
+
+
+    private String displaceName;
+
+    PlaceType(String displaceName) {
+        this.displaceName = displaceName;
+    }
+
+    public String getDisplaceName(){
+        return displaceName;
+    }
+
 }
