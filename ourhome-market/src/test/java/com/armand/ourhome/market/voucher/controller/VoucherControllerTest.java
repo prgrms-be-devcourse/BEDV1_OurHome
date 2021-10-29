a27f365beb6797333567ddcb9db8f77ba4e0c0e0
yekyeong <kwonliz6.6@gmail.com>
2021-10-29 20:21:34 +0900

    [M] test : controller 테스트 코드 작성 (#14)

--- ourhome-market/src/test/java/com/armand/ourhome/market/voucher/controller/VoucherControllerTest.java
+++ ourhome-market/src/test/java/com/armand/ourhome/market/voucher/controller/VoucherControllerTest.java
@@ -1,10 +1,64 @@
 package com.armand.ourhome.market.voucher.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import com.armand.ourhome.market.voucher.dto.VoucherDto;
+import com.armand.ourhome.market.voucher.dto.VoucherType;
+import com.armand.ourhome.market.voucher.service.VoucherService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-@SpringBootTest
+@AutoConfigureRestDocs
+@WebMvcTest
+@ContextConfiguration(classes = VoucherController.class)
 class VoucherControllerTest {
+
+  @Autowired
+  ObjectMapper objectMapper;
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private VoucherService voucherService;
+
+  @Test
+  void testSaveVoucher() throws Exception {
+    // given
+    VoucherDto voucherResponse = VoucherDto.builder()
+        .id(1L)
+        .value(20)
+        .minLimit(30000)
+        .voucherType(VoucherType.PERCENT)
+        .build();
+
+    given(voucherService.save(any())).willReturn(voucherResponse);
+
+    // when
+    ResultActions resultActions = mockMvc.perform(post("/api/vouchers")
+            .content(objectMapper.writeValueAsString(voucherResponse))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print());
+
+    // then
+    resultActions.andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("id").value(1L));
+  }
 
 }
