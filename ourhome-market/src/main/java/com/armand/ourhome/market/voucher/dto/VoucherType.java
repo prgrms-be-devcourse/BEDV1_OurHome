d6770b6c4301f83b792b3a5236e46a25448ccb3e
yekyeong <kwonliz6.6@gmail.com>
2021-10-31 00:00:20 +0900

    [M] feat : 바우처를 수정할 수 있다 - service, controller (#26)

--- ourhome-market/src/main/java/com/armand/ourhome/market/voucher/dto/VoucherType.java
+++ ourhome-market/src/main/java/com/armand/ourhome/market/voucher/dto/VoucherType.java
@@ -4,18 +4,26 @@ import com.armand.ourhome.market.voucher.domain.FixedVoucher;
 import com.armand.ourhome.market.voucher.domain.PercentVoucher;
 import com.armand.ourhome.market.voucher.domain.Voucher;
 
-public enum VoucherType { // 214page
+public enum VoucherType {
   FIXED {
     public FixedVoucher of(int value, int minLimit) {
       return FixedVoucher.of(value, minLimit);
+    }
+    public boolean isDifferentType(Voucher voucher){
+      return FixedVoucher.class != voucher.getClass();
     }
   },
   PERCENT {
     public PercentVoucher of(int value, int minLimit) {
       return PercentVoucher.of(value, minLimit);
     }
+    public boolean isDifferentType(Voucher voucher){
+      return PercentVoucher.class != voucher.getClass();
+    }
   };
 
   public abstract <T extends Voucher> T of(int value, int minLimit);
+
+  public abstract boolean isDifferentType(Voucher voucher);
 
 }
