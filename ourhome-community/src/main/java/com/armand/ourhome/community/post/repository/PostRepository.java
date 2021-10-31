5d609763fb8d39540935e86491fab186d20fe3b3
yunyun <locodingve@gmail.com>
2021-10-31 23:02:08 +0900

    feat: 모든 게시물 조회(filter:기본 및 거주형태)(#40), 상세조회(#37), 특정 게시물 삭제(#38)

--- ourhome-community/src/main/java/com/armand/ourhome/community/post/repository/PostRepository.java
+++ ourhome-community/src/main/java/com/armand/ourhome/community/post/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.armand.ourhome.community.post.repository;
 import com.armand.ourhome.community.post.entity.PlaceType;
 import com.armand.ourhome.community.post.entity.Post;
 import com.armand.ourhome.community.post.entity.ResidentialType;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,7 +14,7 @@ import java.util.List;
  */
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    List<Post> findAllByResidentialType(ResidentialType residentialType);
+    List<Post> findAllByResidentialType(ResidentialType residentialType, Pageable pageable);
 
 
 }
