cfc75bd0f59bb2f23d8ba49e6c1f6ae284aea424
seunghun <dau4723@gmail.com>
2021-11-01 17:12:19 +0900

    [M] chore: 구매자만 리뷰 생성하도록 변경

--- ourhome-market/src/main/java/com/armand/ourhome/market/order/repository/OrderItemRepository.java
+++ ourhome-market/src/main/java/com/armand/ourhome/market/order/repository/OrderItemRepository.java
@@ -3,6 +3,15 @@ package com.armand.ourhome.market.order.repository;
 import com.armand.ourhome.market.order.domain.Order;
 import com.armand.ourhome.market.order.domain.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    @Query("select exist (select i from OrderItem i " +
+            "inner join Order o on i.order = o " +
+            "where o.user = :userId and i.item = :itemId)")
+    boolean existsOrderItemByUserIdAndItemId(@Param("userId") Long userId, @Param("itemId") Long itemId);
 }
--- ourhome-market/src/main/java/com/armand/ourhome/market/review/service/ReviewService.java
+++ ourhome-market/src/main/java/com/armand/ourhome/market/review/service/ReviewService.java
@@ -1,18 +1,22 @@
 package com.armand.ourhome.market.review.service;
 
-import com.armand.ourhome.common.error.exception.EntityNotFoundException;
 import com.armand.ourhome.common.error.exception.InvalidValueException;
 import com.armand.ourhome.domain.item.domain.Item;
 import com.armand.ourhome.domain.item.repository.ItemRepository;
 import com.armand.ourhome.domain.user.User;
 import com.armand.ourhome.domain.user.UserRepository;
+import com.armand.ourhome.market.item.exception.ItemNotFoundException;
+import com.armand.ourhome.market.order.exception.UserNotFoundException;
+import com.armand.ourhome.market.order.repository.OrderItemRepository;
+import com.armand.ourhome.market.review.domain.Aggregate;
 import com.armand.ourhome.market.review.domain.Review;
+import com.armand.ourhome.market.review.exception.UserAccessDeniedException;
 import com.armand.ourhome.market.review.mapper.ReviewMapper;
 import com.armand.ourhome.market.review.repository.ReviewRepository;
-import com.armand.ourhome.market.review.service.dto.request.RequestAddReview;
-import com.armand.ourhome.market.review.service.dto.request.RequestReviewPages;
-import com.armand.ourhome.market.review.service.dto.response.PageResponse;
-import com.armand.ourhome.market.review.service.dto.response.ResponseReview;
+import com.armand.ourhome.market.review.dto.request.RequestAddReview;
+import com.armand.ourhome.market.review.dto.request.RequestReviewPages;
+import com.armand.ourhome.market.review.dto.response.PageResponse;
+import com.armand.ourhome.market.review.dto.response.ResponseReview;
 import lombok.RequiredArgsConstructor;
 import org.mapstruct.factory.Mappers;
 import org.springframework.data.domain.Page;
@@ -32,6 +36,8 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final UserRepository userRepository;
     private final ItemRepository itemRepository;
+    private final OrderItemRepository orderItemRepository;
+
 
     @Transactional
     public Long save(RequestAddReview request) {
@@ -52,8 +58,13 @@ public class ReviewService {
         return new PageResponse<>(pages.getTotalElements(), pages.getTotalPages(), reviews, pages.getSize());
     }
 
+    public Aggregate getReviewAggregateBy(Item item) {
+        return reviewRepository.countReviewAndAverageRatingOf(item);
+    }
+
     private Review createReview(RequestAddReview request) {
-        validAlreadyExists(request.getItemId(), request.getUserId());
+        validUserOrderedItem(request.getItemId(), request.getUserId());
+        validReviewAlreadyExists(request.getItemId(), request.getUserId());
 
         User user = getUser(request.getUserId());
         Item item = getItem(request.getItemId());
@@ -63,15 +74,23 @@ public class ReviewService {
 
     private Item getItem(Long itemId) {
         return itemRepository.findById(itemId)
-                .orElseThrow(() -> new EntityNotFoundException(MessageFormat.format("해당 상품이 존재하지 않습니다. itemId = {0}", itemId)));
+                .orElseThrow(() -> new ItemNotFoundException(MessageFormat.format("해당 상품이 존재하지 않습니다. itemId = {0}", itemId)));
     }
 
     private User getUser(Long userId) {
         return userRepository.findById(userId)
-                .orElseThrow(() -> new EntityNotFoundException(MessageFormat.format("해당 사용자는 존재하지 않습니다. userId = {0}", userId)));
+                .orElseThrow(() -> new UserNotFoundException(MessageFormat.format("해당 사용자는 존재하지 않습니다. userId = {0}", userId)));
     }
 
-    private void validAlreadyExists(Long itemId, Long userId) {
+    private void validUserOrderedItem(Long itemId, Long userId) {
+        boolean exists = orderItemRepository.existsOrderItemByUserIdAndItemId(itemId, userId);
+
+        if (!exists) {
+            throw new UserAccessDeniedException(MessageFormat.format("상품을 구매하지 않는 경우 리뷰를 남길 수 없습니다. itemId = {0}, userId = {1}", itemId, userId));
+        }
+    }
+
+    private void validReviewAlreadyExists(Long itemId, Long userId) {
         boolean exists = reviewRepository.existsByItemIdAndUserId(itemId, userId);
 
         if (exists) {
