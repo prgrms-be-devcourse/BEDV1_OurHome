fb1a6ce998d73aaef45a9423fbc4cf08b32fd136
yekyeong <kwonliz6.6@gmail.com>
2021-11-03 02:38:18 +0900

    [M] feat : WalletNotFoundException 구체적인 예외클래스 생성 (#61)

---
+++ ourhome-market/src/main/java/com/armand/ourhome/market/voucher/exception/WalletNotFoundException.java
@@ -0,0 +1,11 @@
+package com.armand.ourhome.market.voucher.exception;
+
+import com.armand.ourhome.common.error.exception.EntityNotFoundException;
+
+public class WalletNotFoundException extends EntityNotFoundException {
+
+  public WalletNotFoundException(String message) {
+    super(message);
+  }
+
+}
