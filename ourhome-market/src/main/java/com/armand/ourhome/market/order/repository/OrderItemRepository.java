fd79451b436bb8aa644a1b5c11309a47221efb11
seunghun <dau4723@gmail.com>
2021-11-03 15:08:47 +0900

    fix: .gitignore 적용

--- ourhome-market/src/main/java/com/armand/ourhome/market/order/repository/OrderItemRepository.java
+++ ourhome-market/src/main/java/com/armand/ourhome/market/order/repository/OrderItemRepository.java
@@ -9,9 +9,8 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-    // limit 1로 하면 성능 더 좋지만, JPQL limit 지원안함
-    @Query("select count(i) > 0 from OrderItem i " +
-            "join i.order o " +
-            "where o.user.id = :userId and i.item.id = :itemId")
+    @Query(value = "select count(*) > 0 from order_item i " +
+            "join orders o on i.order_id = o.id " +
+            "where o.user_id = :userId and i.item_id = :itemId limit 1", nativeQuery = true)
     boolean existsOrderItemByUserIdAndItemId(@Param("userId") Long userId, @Param("itemId") Long itemId);
 }
