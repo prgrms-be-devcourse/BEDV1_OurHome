0ad797179de00009339842b6cae647fa127a5679
yekyeong <kwonliz6.6@gmail.com>
2021-11-04 22:02:21 +0900

    test : 바우처 사용해 주문생성 테스트 코드 작성 (#61)

--- ourhome-market/src/test/java/com/armand/ourhome/market/TestHelper.java
+++ ourhome-market/src/test/java/com/armand/ourhome/market/TestHelper.java
@@ -9,80 +9,76 @@ import com.armand.ourhome.market.order.domain.PaymentType;
 import com.armand.ourhome.market.order.dto.OrderItemRequest;
 import com.armand.ourhome.market.order.dto.OrderRequest;
 
+import com.armand.ourhome.market.voucher.domain.FixedVoucher;
+import com.armand.ourhome.market.voucher.domain.PercentVoucher;
+import com.armand.ourhome.market.voucher.domain.Voucher;
+import com.armand.ourhome.market.voucher.domain.Wallet;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TestHelper {
 
-    public static User createUserWithoutAddress() {
-        return User.builder()
-                .email("iyj6707@naver.com")
-                .password("1234")
-                .nickname("iyj6707")
-                .build();
-    }
+  public static User createUserWithoutAddress() {
+    return User.builder()
+        .email("iyj6707@naver.com")
+        .password("1234")
+        .nickname("iyj6707")
+        .build();
+  }
 
-    public static User createUser() {
-        return User.builder()
-                .email("iyj6707@gmail.com")
-                .password("1234")
-                .nickname("iyj67071")
-                .address("Seoul city")
-                .build();
-    }
+  public static User createUser() {
+    return User.builder()
+        .email("iyj6707@gmail.com")
+        .password("1234")
+        .nickname("iyj67071")
+        .address("Seoul city")
+        .build();
+  }
 
-    public static Item createSoapItem() {
-        return Item.builder()
-                .price(1000)
-                .name("soap")
-                .imageUrl("example")
-                .description("clean")
-                .stockQuantity(4)
-                .company(new Company("samsung"))
-                .category(Category.DAILY_SUPPLIES)
-                .build();
-    }
+  public static Item createSoapItem() {
+    return Item.builder()
+        .price(1000)
+        .name("soap")
+        .imageUrl("example")
+        .description("clean")
+        .stockQuantity(4)
+        .company(new Company("samsung"))
+        .category(Category.DAILY_SUPPLIES)
+        .build();
+  }
 
-    public static Item createRamenitem() {
-        return Item.builder()
-                .price(1500)
-                .name("ramen")
-                .imageUrl("example")
-                .description("yummy")
-                .stockQuantity(3)
-                .company(new Company("samyang"))
-                .category(Category.ETC)
-                .build();
-    }
+  public static Item createRamenitem() {
+    return Item.builder()
+        .price(1500)
+        .name("ramen")
+        .imageUrl("example")
+        .description("yummy")
+        .stockQuantity(3)
+        .company(new Company("samyang"))
+        .category(Category.ETC)
+        .build();
+  }
 
-    private static List<OrderItemRequest> createOrderItemRequests() {
-        List<OrderItemRequest> orderItemRequests = new ArrayList<>();
-        orderItemRequests.add(OrderItemRequest.builder()
-                .orderCount(2)
-                .itemId(1L)
-                .build());
-        orderItemRequests.add(OrderItemRequest.builder()
-                .orderCount(1)
-                .itemId(2L)
-                .build());
+  private static List<OrderItemRequest> createOrderItemRequests() {
+    List<OrderItemRequest> orderItemRequests = new ArrayList<>();
+    orderItemRequests.add(OrderItemRequest.builder()
+        .orderCount(2)
+        .itemId(1L)
+        .build());
+    orderItemRequests.add(OrderItemRequest.builder()
+        .orderCount(1)
+        .itemId(2L)
+        .build());
 
-        return orderItemRequests;
-    }
+    return orderItemRequests;
+  }
 
-    public static OrderRequest createOrderRequest() {
-        return OrderRequest.builder()
-                .paymentType(PaymentType.FUND_TRANSFER)
-                .userId(1L)
-                .address("Seoul City")
-                .orderItemRequests(createOrderItemRequests())
-                .build();
-    }
+  public static PercentVoucher createPercentVoucher(int percent, int minLimit) {
+    return PercentVoucher.of(percent, minLimit);
+  }
 
-    public static OrderRequest createOrderRequestWithoutAddress() {
-        return OrderRequest.builder()
-                .paymentType(PaymentType.FUND_TRANSFER)
-                .userId(1L)
-                .orderItemRequests(createOrderItemRequests())
-                .build();
-    }
+  public static Wallet createWallet(User user, Voucher voucher) {
+    return Wallet.of(user, voucher);
+  }
+
 }
