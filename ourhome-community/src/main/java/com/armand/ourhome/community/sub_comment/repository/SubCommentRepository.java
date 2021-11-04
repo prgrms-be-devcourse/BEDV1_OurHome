2810a1a02b70fea876db571321279c0938b6e9ce
tnsy <soo5717@sookmyung.ac.kr>
2021-11-05 03:10:10 +0900

    feat: SubCommentRepository 구현

---
+++ ourhome-community/src/main/java/com/armand/ourhome/community/sub_comment/repository/SubCommentRepository.java
@@ -0,0 +1,8 @@
+package com.armand.ourhome.community.sub_comment.repository;
+
+import com.armand.ourhome.community.sub_comment.entity.SubComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubCommentRepository extends JpaRepository<SubComment, Long> {
+
+}
