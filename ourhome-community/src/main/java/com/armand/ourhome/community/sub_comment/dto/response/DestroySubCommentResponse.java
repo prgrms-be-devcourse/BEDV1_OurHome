f1ab7c20c179c2ac68c9d9052fa14f0af4342f7d
tnsy <soo5717@sookmyung.ac.kr>
2021-11-05 06:58:19 +0900

    feat: DestroySubCommentMapper, Response 구현

---
+++ ourhome-community/src/main/java/com/armand/ourhome/community/sub_comment/dto/response/DestroySubCommentResponse.java
@@ -0,0 +1,11 @@
+package com.armand.ourhome.community.sub_comment.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class DestroySubCommentResponse {
+
+    private Long destroySubCommentId;
+}
