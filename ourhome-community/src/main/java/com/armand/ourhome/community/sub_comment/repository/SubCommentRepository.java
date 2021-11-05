2b3d51e95faeec104c84c8719387c62f283875d7
Suyeon <koo5717@naver.com>
2021-11-05 10:02:46 +0900

    [C/sub-comment-104] findByComment -> countByComment로 변경 (#105)
    * refactor: findByComment -> countByComment로 변경
    
    * refactor: CommentService, SubCommentService countByComment로 변경

--- ourhome-community/src/main/java/com/armand/ourhome/community/sub_comment/repository/SubCommentRepository.java
+++ ourhome-community/src/main/java/com/armand/ourhome/community/sub_comment/repository/SubCommentRepository.java
@@ -2,10 +2,9 @@ package com.armand.ourhome.community.sub_comment.repository;
 
 import com.armand.ourhome.community.comment.entity.Comment;
 import com.armand.ourhome.community.sub_comment.entity.SubComment;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubCommentRepository extends JpaRepository<SubComment, Long> {
 
-    List<SubComment> findByComment(Comment comment);
+    Long countByComment(Comment comment);
 }
